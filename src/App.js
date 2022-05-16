731896ac3e9587ce3a5d252a76eb0c9c37ea661a
jandcreative <javi.diaz@jandcreative.com>
2022-05-16 22:12:41 +0200

    Add React Router Library

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home.jsx";
+import Tracks from "./pages/Tracks/Tracks.jsx";
+import Bikers from "./pages/Bikers/Bikers.jsx";
+import Error from "./pages/Error/Error.jsx";
+import Navbar from "./components/Navbar/Navbar";
+import './Reset.css';
 
 function App() {
   return (
     <div className="App">
-      <div className="content">
-        <h1>Pirinexus is coming!</h1>
-      </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/tracks" element={<Tracks />} />
+        <Route path="/bikers" element={<Bikers />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+      <Navbar />
     </div>
   );
 }
