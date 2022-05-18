ad6e80a90478a9c0643e8727b7d5b18be97b89a8
jandcreative <javi.diaz@jandcreative.com>
2022-05-18 17:35:59 +0200

    Navbar format update

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,21 +1,17 @@
 import { NavLink } from "react-router-dom";
-import './Navbar.css';
+import "./Navbar.css";
 
 function Navbar() {
   return (
-
     <header>
-
-        <div className="logo">
-            <h1>pirineXus</h1>
-        </div>
-
-        <nav>
-            <NavLink to="/" className={({ isActive }) => (isActive ? "selected" : "")}>Home</NavLink>
-            <NavLink to="/tracks" className={({ isActive }) => (isActive ? "selected" : "")}>Tracks</NavLink>
-            <NavLink to="/bikers" className={({ isActive }) => (isActive ? "selected" : "")}>Bikers</NavLink>
-        </nav>
-
+      <div className="logo">
+        <h1>pirineXus</h1>
+      </div>
+      <nav>
+        <NavLink to="/" className={({ isActive }) => (isActive ? "selected" : "")}> Home </NavLink>
+        <NavLink to="/tracks" className={({ isActive }) => (isActive ? "selected" : "")}>Tracks</NavLink>
+        <NavLink to="/bikers" className={({ isActive }) => (isActive ? "selected" : "")}>Bikers</NavLink>
+      </nav>
     </header>
   );
 }
