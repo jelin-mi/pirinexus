67d9d680c52ab4bb2f6fccfc7df3e231380cf770
jandcreative <javi.diaz@jandcreative.com>
2022-05-23 23:00:39 +0200

    Create Herohome Component

---
+++ src/components/Herohome/Herohome.jsx
@@ -0,0 +1,28 @@
+import "./Herohome.css";
+import { Link } from "react-router-dom";
+
+function Herohome() {
+  return (
+    <div className="hero-home">
+      <div className="container">
+        <span>Aventura en 4 ruedas</span>
+        <h2>Pirinexus Challenge</h2>
+        <div className="button-container">
+          <button>
+            <Link className="button" to="#">
+              Saber más
+            </Link>
+          </button>
+
+          <button>
+            <Link className="button secondary" to="/bikers">
+              Participantes
+            </Link>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Herohome;
