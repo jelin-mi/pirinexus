9d673b21c3b7af8450b5ed1c9d401f5d0ddebd12
“jelin.mi” <jelin.mi@gmail.com>
2022-05-24 21:38:37 +0200

    remove duplicated App file, css added

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,24 @@
+import "./Reset.css";
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home.jsx";
 import Tracks from "./pages/Tracks/Tracks.jsx";
 import Bikers from "./pages/Bikers/Bikers.jsx";
 import Error from "./pages/Error/Error.jsx";
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/tracks" element={<Tracks />} />
         <Route path="/bikers" element={<Bikers />} />
         <Route path="*" element={<Error />} />
       </Routes>
+      <Footer />
     </div>
   );
 }
