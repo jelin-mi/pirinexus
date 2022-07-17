3ef979754451abe9809e9d627c1f16c9363c1629
jandcreative <javi.diaz@jandcreative.com>
2022-07-17 10:55:27 +0200

    Add Tracks Page

--- src/pages/Tracks/Tracks.jsx
+++ src/pages/Tracks/Tracks.jsx
@@ -1,7 +1,13 @@
+import Footer from "../../components/Footer/Footer";
+import RouteList from "../../components/Routelist/RouteList";
+
 function Tracks() {
+  
   return (
     <>
       <h1>Tracks</h1>
+      <RouteList/>
+      <Footer/>
     </>
   );
 }
