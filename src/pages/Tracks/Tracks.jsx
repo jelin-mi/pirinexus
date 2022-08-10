b33572370638c3646ad1c77f3c134e7cd33cc0fd
“jelin.mi” <jelin.mi@gmail.com>
2022-08-10 21:51:36 +0200

    desnivel img added, intro text updated

--- src/pages/Tracks/Tracks.jsx
+++ src/pages/Tracks/Tracks.jsx
@@ -1,4 +1,5 @@
 import "./Tracks.css";
+import desnivel from "../../assets/images/desnivel.png";
 import TrackMap from "../../components/TrackMap/TrackMap";
 import Banner from "../../components/Banner/Banner";
 import Footer from "../../components/Footer/Footer";
@@ -10,16 +11,19 @@ function Tracks() {
         <h1>Tracks - Rutas</h1>
       </div>
       <p>
-        Pirinexus es una ruta circular de <strong>350 kms</strong> atravesando
-        España (80%) y Francia (20%), idealmente con la bici Gravel.
+        <strong>Pirinexus</strong> es una ruta circular de{" "}
+        <strong>350 kms</strong> atravesando <strong>España</strong> (80%) y{" "}
+        <strong>Francia</strong> (20%), idealmente con la bici Gravel.
         <br />
         Está recomendado hacerla por etapas y no superar 60 km/día. Como su
-        comienzo se considera la ciudad de Girona y hay etapas establecidas pero
-        en realidad depende de cada uno. Pero sí que se recomienda pedalear en
-        el sentido de las agujas del reloj para que la Tramuntana sople a favor
-        del ciclista.
+        comienzo se considera la ciudad de <strong>Girona</strong> y hay etapas
+        establecidas pero en realidad depende de cada uno. Pero sí que se
+        recomienda pedalear en el sentido de las agujas del reloj para que la
+        Tramuntana sople a favor del ciclista.
       </p>
       <TrackMap />
+      <h2>Desnivel</h2>
+      <img src={desnivel} alt="desnivel" className="desnivel" />
       <Banner />
       <Footer />
     </>
